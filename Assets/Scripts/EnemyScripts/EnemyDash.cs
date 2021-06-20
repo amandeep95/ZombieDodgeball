3af287aa365ed460fd8f4e8ae5e1bcd85f6c8828
amandeep <amandeepmalhi21@hotmail.com>
2021-06-20 01:14:13 -0700

    Update June 19
    Increase in overall player speed for better game feel. Dahs now feels like more of a dash.
    
    Creation of new enemy type  - Hunters. Grey circles that dash towards the player once they are close enough. Their overall move speed is faster than the chasers. When they dash they get closer to the player but to not get close enough to hit them

--- Assets/Scripts/EnemyScripts/EnemyDash.cs
+++ Assets/Scripts/EnemyScripts/EnemyDash.cs
@@ -1,18 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyDash : MonoBehaviour
 {
+    private GameObject player;
+    private AIPath enemyPathFind;
+    private bool canmove, canDash;
+    public float enemyDashForce, enemyDashDuration;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
+        enemyPathFind = gameObject.GetComponent<AIPath>();
+        canmove = true;
+        canDash = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemyPathFind.enabled = canmove;
+
+
+        //when P is pressed on keybaord stop moving and dash towards the player then turn on moving again
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            canmove = !canmove;
+        }
+
+        float distanceToPlayer = CalcDistance(player, gameObject);
+        Vector3 directionToPlayer = CalcDirection();
+
+
+        if ((distanceToPlayer <= 7f) && (canDash))
+        {
+            //print("EnemyIsCloseNow");
+            canmove = !canmove;
+            StartCoroutine(DashToPlayer(gameObject.GetComponent<Rigidbody2D>(), directionToPlayer));
+            canmove = !canmove;
+            canDash = false;
+            //return;
+        }
+        else if (distanceToPlayer > 7f)
+        {
+            canDash = true;
+        }
+    }
+
+    float CalcDistance(GameObject player, GameObject enemy)
+    {
+        float dist = Vector2.Distance(player.transform.position, enemy.transform.position);
+        return dist;
+    }
+
+    Vector3 CalcDirection()
+    {
+        Vector3 dir = player.transform.position - transform.position;
+        return dir;
+    }
+
+    IEnumerator DashToPlayer(Rigidbody2D rb, Vector3 dir)
+    {
+        rb.AddForce(dir * (enemyDashForce * 100), ForceMode2D.Force);
+        print("ENEMYDASHED");
+        //canDash = false;
+        yield return new WaitForSeconds(enemyDashDuration);
     }
 }
--- Assets/Scripts/EnemyScripts/EnemySpawn.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawn.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemySpawn : MonoBehaviour
 {
-    public GameObject enemy;
+    public GameObject enemy, enemy2;
     private float countDownTimer;
     public float randMin, randMax;
 
@@ -20,8 +20,23 @@ public class EnemySpawn : MonoBehaviour
         countDownTimer -= Time.deltaTime;
         if (countDownTimer <= 0)
         {
-            Instantiate(enemy, transform.position, Quaternion.identity);
-            countDownTimer = Random.Range(randMin, randMax);
+            float enemyType = Random.Range(1, 3);
+
+            if (enemyType % 3 == 0)
+            {
+                Instantiate(enemy, transform.position, Quaternion.identity);
+                countDownTimer = Random.Range(randMin, randMax);
+            } else if (enemyType % 3 == 1)
+            {
+                Instantiate(enemy2, transform.position, Quaternion.identity);
+                countDownTimer = Random.Range(randMin, randMax);
+            } else if (enemyType % 3 == 2)
+            {
+                //instantate more of enemy 1 than enemy 2 (placeholder for 3rd enemy type)
+                Instantiate(enemy, transform.position, Quaternion.identity);
+                countDownTimer = Random.Range(randMin, randMax);
+            }
+            
         }
     }
 }
